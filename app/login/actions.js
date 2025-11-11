438e93b74eb9d2fa923c64086928675ea78b1570
abhishek <abhishukla99111@gmail.com>
2025-11-11 16:02:43 +0530

    Update actions in login, signup, and lib

--- app/login/actions.js
+++ app/login/actions.js
@@ -26,7 +26,7 @@ export async function login(_, formData) {
   const payload = validatedFields.data;
 // console.log('payload is ',payload);
   try {
-    const res = await fetch('http://localhost:3000/api/auth/login', {
+    const res = await fetch('/api/auth/login', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(payload),
--- app/signup/actions.js
+++ app/signup/actions.js
@@ -34,7 +34,7 @@ const validatedFields = SignupFormSchema.safeParse({
    
    
    try {
-    const res = await fetch('http://localhost:3000/api/auth/signup', {
+    const res = await fetch('/api/auth/signup', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(payload),
